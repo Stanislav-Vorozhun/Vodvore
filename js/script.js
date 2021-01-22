857934d89fcc561b4e9f60eb7545b0d94dc7913c
Stanislav-Vorozhun <stanislav.vorozhun@gmail.com>
2021-01-22 16:57:35 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Slider header
+
+$(document).ready(function() {
+    $('.intro-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        fade: true,
+        arrows: false
+    });
+});
