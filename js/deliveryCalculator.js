fadea3d224f2a336c42a63cc5e9e351ed4bd6ab7
Stanislav-Vorozhun <stanislav.vorozhun@gmail.com>
2021-02-01 15:34:08 +0300

    new map yandex

---
+++ js/deliveryCalculator.js
@@ -0,0 +1,75 @@
+ymaps.ready(init);
+
+function init() {
+    // Стоимость за километр.
+    var DELIVERY_TARIFF = 0.8,
+        // Минимальная стоимость.
+        MINIMUM_COST = 30,
+        myMap = new ymaps.Map("yamap ", {
+            center: [53.816977, 27.572643],
+            zoom: 9,
+            controls: []
+        }),
+        // Создадим панель маршрутизации.
+        routePanelControl = new ymaps.control.RoutePanel({
+            options: {
+                // Добавим заголовок панели.
+                showHeader: true,
+                title: 'Расчёт доставки'
+            }
+        }),
+        zoomControl = new ymaps.control.ZoomControl({
+            options: {
+                size: 'small',
+                float: 'none',
+                position: {
+                    bottom: 145,
+                    right: 10
+                }
+            }
+        });
+    // Пользователь сможет построить только автомобильный маршрут.
+    routePanelControl.routePanel.options.set({
+        types: { auto: true }
+    });
+
+    // Если вы хотите задать неизменяемую точку "откуда", раскомментируйте код ниже.
+    routePanelControl.routePanel.state.set({
+        fromEnabled: false,
+        from: '53.816977, 27.572643'
+    });
+
+    myMap.controls.add(routePanelControl).add(zoomControl);
+
+    // Получим ссылку на маршрут.
+    routePanelControl.routePanel.getRouteAsync().then(function(route) {
+
+        // Зададим максимально допустимое число маршрутов, возвращаемых мультимаршрутизатором.
+        route.model.setParams({ results: 1 }, true);
+
+        // Повесим обработчик на событие построения маршрута.
+        route.model.events.add('requestsuccess', function() {
+
+            var activeRoute = route.getActiveRoute();
+            if (activeRoute) {
+                // Получим протяженность маршрута.
+                var length = route.getActiveRoute().properties.get("distance"),
+                    // Вычислим стоимость доставки.
+                    price = calculate(Math.round(length.value / 1000)),
+                    // Создадим макет содержимого балуна маршрута.
+                    balloonContentLayout = ymaps.templateLayoutFactory.createClass(
+                        '<span>Расстояние: ' + length.text + '.</span><br/>' +
+                        '<span style="font-weight: bold; font-style: italic">Стоимость доставки: ' + price + ' BYN.</span>');
+                // Зададим этот макет для содержимого балуна.
+                route.options.set('routeBalloonContentLayout', balloonContentLayout);
+                // Откроем балун.
+                activeRoute.balloon.open();
+            }
+        });
+
+    });
+    // Функция, вычисляющая стоимость доставки.
+    function calculate(routeLength) {
+        return Math.max(routeLength * DELIVERY_TARIFF, MINIMUM_COST);
+    }
+}
